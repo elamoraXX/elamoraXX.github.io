5eb8d07a5f261a76c5df6fa4eba2646ed7ebb85b
elamoraXX <56541094+elamoraXX@users.noreply.github.com>
2019-10-15 16:46:20 +0800

    Update and rename js/login.js to  js/login.js

--- js/login.js
+++ js/login.js
@@ -1,117 +1,96 @@
+window.onload = function() {
+			function $(param) {
+				if (arguments[1] == true) {
+					return document.querySelectorAll(param);
+				} else {
+					return document.querySelector(param);
+				}
+			}
+			var $box = $(".box");
+			var $box1 = $(".box-1 ul li", true);
+			var $box2 = $(".box-2 ul");
+			var $box3 = $(".box-3");
+			var $length = $box1.length;
 
-  var errori ='<%=request.getParameter("error")%>';
-  if(errori=='yes'){
-   alert("登录失败,账号或密码错误,请检查!");
-  }
-	var elmora = "elmora";
-	var elmorapwd = "123456";
-	window.onload=function(){
-    	createCode();
-    }
-	function dlsb(){
-		
-	}
-	function yz(){
-		var username = document.getElementById("user_name").value;
-		var userpwd = document.getElementById("user_pwd").value;
-		if(username!=elmora && userpwd!=elmorapwd){
-			alert("账号或密码错误！");
-			return false;
-		}
-		if(username==elmora && userpwd==elmorapwd){
-			localStorage.setItem('key',username);
-			return yzUsername() && yzPassword() && yzyzm();
-		}
-		alert("账号或密码错误！")
-		return false;
-	}
+			var str = "";
+			for (var i = 0; i < $length; i++) {
+				if (i == 0) {
+					str += "<li class='on'>" + (i + 1) + "</li>";
+				} else {
+					str += "<li>" + (i + 1) + "</li>";
+				}
+			}
+			$box2.innerHTML = str;
 
-	function PwdLogin() {
-	    var login = document.getElementsByClassName("login_con");
-	    login[0].classList.remove("hidden");
-	    login[0].classList.add("show");
-	    login[1].classList.remove("show");
-	    login[1].classList.add("hidden");
-	    var tags = document.getElementsByClassName("top_tag");
-	    tags[0].classList.add("active");
-	    tags[1].classList.remove("active");
-	    var ad = document.getElementById("AdImg");
-	    ad.style.height = "558px";
-	}
-	function QrcodeLogin() {
-	    var login = document.getElementsByClassName("login_con");
-	    login[0].classList.remove("show");
-	    login[0].classList.add("hidden");
-	    login[1].classList.remove("hidden");
-	    login[1].classList.add("show");
-	    var tags = document.getElementsByClassName("top_tag");
-	    tags[1].classList.add("active");
-	    tags[0].classList.remove("active");
-	    var ad = document.getElementById("AdImg");
-	    ad.style.height = "407px";
-	}
+			var current = 0;
 
-	function yzPassword(){
-		var pwdValue = document.getElementById("user_pwd").value;
-		var pwdspan = document.getElementById("passwordspan");
-		if(pwdValue==""){
-			pwdspan.innerHTML = "请检查密码,密码不能为空";
-			return false;
-		}else{
-			pwdspan.innerHTML = "";
-			return true;
+			var timer;
+			timer = setInterval(go, 1000);
+
+			function go() {
+				for (var j = 0; j < $length; j++) {
+					$box1[j].style.display = "none";
+					$box2.children[j].className = "";
+				}
+				if ($length == current) {
+					current = 0;
+				}
+				$box1[current].style.display = "block";
+				$box2.children[current].className = "on";
+				current++;
+			}
+
+			for (var k = 0; k < $length; k++) {
+				$box1[k].onmouseover = function() {
+					clearInterval(timer);
+				}
+				$box1[k].onmouseout = function() {
+					timer = setInterval(go, 1000);
+				}
+			}
+			for (var p = 0; p < $box3.children.length; p++) {
+				$box3.children[p].onmouseover = function() {
+					clearInterval(timer);
+				};
+				$box3.children[p].onmouseout = function() {
+					timer = setInterval(go, 1000);
+				}
+			}
+
+			for (var u = 0; u < $length; u++) {
+				$box2.children[u].index = u;
+				$box2.children[u].onmouseover = function() {
+					clearInterval(timer);
+					for (var j = 0; j < $length; j++) {
+						$box1[j].style.display = "none";
+						$box2.children[j].className = "";
+					}
+					this.className = "on";
+					$box1[this.index].style.display = "block";
+					current = this.index + 1;
+				}
+				$box2.children[u].onmouseout = function() {
+					timer = setInterval(go, 1000);
+				}
+			}
+
+			$box3.children[0].onclick = function() {
+				back();
+			}
+			$box3.children[1].onclick = function() {
+				go();
+			}
+
+			function back() {
+				for (var j = 0; j < $length; j++) {
+					$box1[j].style.display = "none";
+					$box2.children[j].className = "";
+				}
+				if (current == 0) {
+					current = $length;
+				}
+				$box1[current - 1].style.display = "block";
+				$box2.children[current - 1].className = "on";
+				current--;
+			}
 		}
-	}
-	function yzUsername(){
-		var usernameInput = document.getElementById("user_name");
-		var usernameValue = usernameInput.value;
-		var usernamespan = document.getElementById("usernamespan");
-		if(usernameValue==""){
-			usernamespan.innerHTML = "请检查用户名,用户名不能为空";
-			return false;
-		}else{
-			usernamespan.innerHTML = "";
-			return true;
-		}
-	}
-	function yzyzm(){
-		 //获取显示区生成的验证码
-        var checkCode = document.getElementById("yzmspan").innerHTML;
-        //获取输入的验证码
-        var inputCode = document.getElementById("auth_code").value;
-		var yzmInput = document.getElementById("auth_code");
-		var yzmValue = yzmInput.value;
-		var yzmspan = document.getElementById("yzmmspan");
-		if(yzmValue == ""){
-			yzmspan.innerHTML = "请检查验证码,验证码不能为空";
-			return false;
-			//toUpperCase()把所有小写字母转换为大写字母
-		}else if(checkCode.toUpperCase() != inputCode.toUpperCase()){
-			yzmspan.innerHTML = "请检查验证码,验证码有误~";
-			return false;
-		}else{
-			yzmspan.innerHTML = "验证码正确";
-			return true;
-		}
-	}
-	function createCode() {
-        var code = "";
-        var codeLength = parseInt(4); //验证码的长度
-        var checkCode = document.getElementById("yzmspan");
-        ////所有候选组成验证码的字符，当然也可以用中文的
-        var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); 
-        //循环组成验证码的字符串
-        for (var i = 0; i < codeLength; i++)
-        {
-            //获取随机验证码下标
-            var charNum = Math.floor(Math.random() * 62);
-            //组合成指定字符验证码
-            code += codeChars[charNum];
-        }
-        if (checkCode)
-        {
-        	checkCode.innerHTML = code;
-        }
-    }
